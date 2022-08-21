9b2aca4e1c8186b2c5d83de31e1825576e53e940
fuyunlong <1273179133@qq.com>
2022-08-22 06:45:30 +0800

    feat: 提交部署内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,43 @@
+// const path = require('path');
+// const fs = require('fs');
+
+// const webglFolder = path.join(__dirname, '../webgl');
+
+// const webgl = fs.readdirSync(webglFolder).filter(md => md !== 'README.md');
+
+// console.log('打印***webgl',webgl)
+
 module.exports = {
-    title: 'Hello VuePress', //标题
-    description: 'Just playing around'
+    title: '一诺滚雪球', //标题
+    description: '前端知识',
+    base: '/FEBlog/',
+    themeConfig: {
+        // sidebar: {
+        //     '/webgl/': [
+        //         'one'
+        //     ],
+        // },
+        sidebar: [
+            {
+                title: 'webgl',   // 必要的
+                path: '/webgl/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/'
+                ]
+            }
+        ],
+        nav: [
+            // 单个地址
+            { text: '首页', link: '/' },
+            // 多个地址
+            {
+                text: '博客地址',
+                items: [
+                    { text: 'Github', link: 'https://github.com/zhangwinwin/FEBlog' },
+                ]
+            }
+        ]
+    }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,24 @@
 # Hello VuePress
 
-## HMR
+---
+home: true
+heroImage: logo.png
+heroText: zhangwinwin blog
+tagline: mostly articles about frontend and webgl
+actionText: Get Started →
+actionLink: /webgl/
+features:
+  - title: network
+    details: 16 articles about network
+  - title: frontend
+    details: 41 articles about frontend
+  - title: webgl
+    details: 4 articles about webgl
+footer: MIT Licensed
+---
+
+作者：zhangwinwin
+链接：https://juejin.cn/post/7069961158899269663
+来源：稀土掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
