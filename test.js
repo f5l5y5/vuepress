2d970f5117abc679b664a769f11b9c418c04cf5d
fuyunlong <1273179133@qq.com>
2022-08-29 07:24:31 +0800

    feat: 数组移动

--- test.js
+++ test.js
@@ -1,42 +1,20 @@
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
- var lengthOfLIS = function(nums) {
-	 let n = nums.length
-	 if(n===0){
-		 return 0
-	 }
- 
-	 let dp = Array(n).fill(1)
-	 for(let i=0;i<n;i++){
-		 for(j=0;j<i;j++){
-			 if(nums[i]>nums[j]){
-				 dp[i] = Math.max(dp[i],dp[j]+1)
-			 }
-		 }
-	 }
-	 return Math.max(...dp)
- };
- const nums = [0,1,0,3,2,3]
- lengthOfLIS(nums)
-
-// [1,1,1,1,1,1]
-//判断1>0 是dp[1] = dp[1],dp[0]+1 [1,2,1,1,1,1]
-//i=2 [1,2,1,1,1,1]
-//i=3 [1,2,1,3,1,1]
-//i=4 => 1,2,1,3,2,1 => 1,2,1,3,3,1
-//i=5 => 1,2,1,3,2,1 => 1,2,1,3,3,1
-
-
-
-
- 	// [0,1,0,3,2,3]
-	//选择极值用dp
-	// dp[i] 在i的位置以前的数组 最长递增子序列的长度
-	// 例如 刚开始是1个数[1,1,1,1,1,1]
-	// 第二个数 nums[i]>nums[i-1] [1,2,1,1,1,1] 
-	// 第三个数 比之前的小  [1,2,2,1,1,1] 
-	// 第四个数 比之前的大  [1,2,2,3,1,1] 
-	// 第五个数 比之前的小  [1,2,2,3,3,1] 
-	// 第六个数 比之前的大  [1,2,2,3,3,4] 
+ var moveZeroes = function(nums) {
+    //先不移动0 最后将后面的数修改为0
+    let fast = 0
+    let slow = 0
+    while(fast<nums.length){
+        if(nums[fast]){
+            let tmp = nums[slow]
+            nums[slow] = nums[fast]
+            nums[fast] = tmp
+            slow++
+        }
+        fast++
+    }
+};
+const arr = [1,2,2,2]
+moveZeroes(arr)
